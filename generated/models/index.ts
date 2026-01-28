7396ec5e949864362ff8747ce53a17cdf899a618
bardonadam <bardon.adam@gmail.com>
2026-01-28 19:36:43 +0100

    chore: add generated baseline

---
+++ generated/models/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ContentStateEnd';
+export * from './ContentStateStart';
+export * from './ContentStateUpdate';
+export * from './LiveActivityEndRequest';
+export * from './LiveActivityEndResponse';
+export * from './LiveActivityLimitError';
+export * from './LiveActivityStartRequest';
+export * from './LiveActivityStartResponse';
+export * from './LiveActivityUpdateRequest';
+export * from './LiveActivityUpdateResponse';
+export * from './PushNotificationRequest';
+export * from './PushNotificationResponse';
+export * from './RateLimitError';
+export * from './SendPushNotification429Response';
